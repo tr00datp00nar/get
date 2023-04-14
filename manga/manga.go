0ad689e0f45b6bccdc4fedec04d056ddaa4ae532
Micah <tr00datp00nar@gmail.com>
2023-04-14 12:11:20 -0400

    Use relative path for manga cmd

--- manga/manga.go
+++ manga/manga.go
@@ -3,11 +3,24 @@ package manga
 import (
 	"os"
 	"os/exec"
+	"os/user"
+	"strings"
+
+	"github.com/charmbracelet/log"
 )
 
 func PullManga() {
-	// TODO: Use a relative path instead of hardcoding the $HOME dir
-	cmd := exec.Command("mangadex-dl", "/home/micah/.config/mangadex-dl/list.txt")
+	user, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	homeDir := user.HomeDir
+	listFile := ".config/mangadex-dl/list.txt"
+	confDir := strings.Join([]string{homeDir, listFile}, "/")
+
+	cmd := exec.Command("mangadex-dl", confDir)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
