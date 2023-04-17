3c5f574d8de967092e9bc376d907ecb19489fdd4
Micah <tr00datp00nar@gmail.com>
2023-04-17 18:11:58 -0400

    Switch from scriptish to tr00datp00nar/fn:ExecBash()

--- weather/weather.go
+++ weather/weather.go
@@ -4,52 +4,20 @@ import (
 	"fmt"
 	"strings"
 
-	scriptish "github.com/ganbarodigital/go_scriptish"
-
 	"github.com/tr00datp00nar/fn"
 )
 
 func getWeather(location string) {
 	location = fn.RawUrlEncode(strings.Title(location))
-	withoutLocation := "https://wttr.in/?format=3"
-	withLocation := "https://wttr.in/" + location + "?format=3"
+	withoutLocation := "https://wttr.in/"
+	withLocation := "https://wttr.in/" + location
 
 	if len(location) > 0 {
-		pipeline := scriptish.NewPipeline(
-			scriptish.Exec("curl", withLocation),
-			scriptish.ToStdout(),
-		)
-		pipeline.Exec()
+		cmd := "curl -L " + withLocation
+		fn.ExecBash(cmd)
 	} else {
-		pipeline := scriptish.NewPipeline(
-			scriptish.Exec("curl", withoutLocation),
-			scriptish.ToStdout(),
-		)
-		pipeline.Exec().Strings()
+		cmd := "curl -L " + withoutLocation
+		fn.ExecBash(cmd)
 		fmt.Println("\nProvide a location string as an argument for more accurate responses.")
 	}
-
-	// if len(location) > 0 {
-	// 	resp, err := http.Get(withLocation)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer resp.Body.Close()
-	// 	body, err := io.Copy(os.Stdout, resp.Body)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println(body)
-	// } else {
-	// 	resp, err := http.Get(withoutLocation)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer resp.Body.Close()
-	// 	body, err := io.Copy(os.Stdout, resp.Body)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println(body)
-	// }
 }
